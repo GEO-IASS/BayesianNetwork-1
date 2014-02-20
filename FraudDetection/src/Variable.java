a1a2fc6509ae4533278b4ff850502900a1dbac1f
Ritchie <Ritchie.Bui@gmail.com>
2014-02-19 19:24:58 -0500

    Added iVars to Variable class

--- FraudDetection/src/Variable.java
+++ FraudDetection/src/Variable.java
@@ -1,4 +1,24 @@
 
-public class Variable {
+public class Variable 
+{
+	private int index;
+	private String desc;
+	
+	
 
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
 }
